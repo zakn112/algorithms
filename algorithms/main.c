5b3bca7080c80a3757abd69c002efdd5c8ea6f50
zakn112 <56603472+zakn112@users.noreply.github.com>
2020-02-18 21:24:21 +0300

    Initial commit

---
+++ algorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  algorithms
+//
+//  Created by Андрей Закусов on 18.02.2020.
+//  Copyright © 2020 Андрей Закусов. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
