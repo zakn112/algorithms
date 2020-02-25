52de8329d7b1302133cda194b22f9be6a6346e2e
zakn112 <56603472+zakn112@users.noreply.github.com>
2020-02-25 21:46:40 +0300

    lesson 2 code

--- algorithms/main.c
+++ algorithms/main.c
@@ -8,8 +8,68 @@
 
 #include <stdio.h>
 
+void decToBin(void);
+int bin(int);
+
+void taskPow(void);
+int powABRec(int, int);
+int powAB(int, int);
+
 int main(int argc, const char * argv[]) {
    
-       
+    //decToBin();
+    taskPow();
+    
     return 0;
+}
+
+
+void decToBin(){
+    int number10;
+    printf("Введите число в десятиричной системе: ");
+    scanf("%i", &number10);
+    
+    printf("Ваше число в двоичной системе: %i \n", bin(number10));
+    
+}
+
+int bin(number10){
+    if (number10 == 0) {
+        return 0;
+    }
+    
+    int res = number10 % 2 + 10 * bin(number10/2);
+    return res;
+}
+
+void taskPow(){
+    int number, degree, numberDeg;
+    printf("Введите число и степень: ");
+    scanf("%i %i", &number, &degree);
+    
+    numberDeg = powAB(number, degree);
+    printf("%i в степени %i = %i \n", number, degree, numberDeg);
+    
+    numberDeg = powABRec(number, degree);
+    printf("%i в степени %i = %i \n", number, degree, numberDeg);
+}
+
+int powAB(number, degree){
+    int numberDeg;
+    numberDeg = number;
+    int i = 1;
+    while (i < degree) {
+        numberDeg *= number;
+        i += 1;
+    }
+    
+    return numberDeg;
+}
+
+int powABRec(number, degree){
+    if (degree == 1) {
+        return number;
+    }
+    
+    return number * powABRec(number, degree - 1);
 }
